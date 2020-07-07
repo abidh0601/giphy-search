e3e9dc05d84468085a9a9a94f1d004b9771c580d
abidh0601 <abidh0601@gmail.com>
2020-07-07 16:16:06 -0400

    Updates

---
+++ precache-manifest.48ba3cc462a0cbd6939a4980c68f197b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26326532f77748e207c9b0b8fa40cf93",
+    "url": "/giphy-search/index.html"
+  },
+  {
+    "revision": "0089604cfbf3c9e36996",
+    "url": "/giphy-search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "61480d531d953599246a",
+    "url": "/giphy-search/static/js/2.f9f9bc4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphy-search/static/js/2.f9f9bc4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0089604cfbf3c9e36996",
+    "url": "/giphy-search/static/js/main.69749636.chunk.js"
+  },
+  {
+    "revision": "3190d8fc86bf058e0668",
+    "url": "/giphy-search/static/js/runtime-main.e53880a0.js"
+  }
+]);
